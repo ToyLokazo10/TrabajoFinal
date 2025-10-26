e6df45b99a0a67edd9285793be136af2fc753152
Steve Vidal <stevenvidalechevarria10@gmail.com>
2025-10-26 18:41:44 -0500

    project start

---
+++ Assets/Scripts/MenuPrinicipal.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public string escenaNivel = "Nivel1";
+    public string escenaCreditos = "Creditos";
+
+    public void Jugar()
+    {
+        SceneManager.LoadScene(escenaNivel);
+    }
+
+    public void Creditos()
+    {
+        SceneManager.LoadScene(escenaCreditos);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("El juego se cerro..");
+    }
+}
